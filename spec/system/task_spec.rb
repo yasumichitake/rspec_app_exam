0134d68021d9f0257827f1739c11696133c8b3af
yasumichitake <t.yasumichi0515@gmail.com>
2021-02-23 23:39:22 +0900

    [FIX]task作成時にプロジェクトが作られるようアソシエーションを定義

--- spec/system/task_spec.rb
+++ spec/system/task_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe 'Task', type: :system do
   let(:project) {create(:project)}
-  let(:task) {create(:task, project_id: project.id)}
+  let(:task) {create(:task)}
   describe 'Task一覧' do
     context '正常系' do
       it '一覧ページにアクセスした場合、Taskが表示されること' do
